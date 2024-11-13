172a48fa87816ba45f0a8c2f99241a7ef44d3965
Hershey Marbeda <126095800+HersheyMarbeda@users.noreply.github.com>
2024-11-13 14:45:53 -0500

    Update M04-1-RandomWords-HBM.cpp

--- Sets-Maps/M04-1-RandomWords-HBM.cpp
+++ Sets-Maps/M04-1-RandomWords-HBM.cpp
@@ -1,1 +1,22 @@
+    // /***************************************************************************
+    //  *                                                                         *
+    //  *   Progammer: Hershey Marbeda                                            *
+    //  *   Instructor: Dr. Carver                                                *
+    //  *   Course: CSCI 202-Data Structures and Algorithms                       *
+    //  *   Due Date: 11.18.2024                                                  *
+    //  *   College: Ivy Tech Community College                                   *
+    //  *                                                                         *
+    //  *   ------------------------- About the Program ------------------------- *
+    //  *   File Name: M04-1-RandomWords-HBM.cpp                                  *
+    //  *   Import Filename: Random-Words.txt                                     *
+    //  *   Program Assignment Name: US States and Capitals                       *
+    //  *   Last Date Updated: 11.13.2024                                         *
+    //  *                                                                         *
+    //  *   Program Assignment Description:                                       *
+    //  *   This program reads the file US-States-With-Capitals.txt               *
+    //  *   and stores the data in a vector of structs.                           *
+    //  *   The program then ask the user to enter a state name and               *
+    //  *   the program will display the full name and capital of the state.      *
+    //  *                                                                         *
+    //  ****************************************************************************/
 
